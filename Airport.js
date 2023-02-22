08c5d755b3ad7849ada531a680be8371ca637db2
kevin-liu <liu.kevin1997@gmail.com>
2023-02-22 11:55:35 -0500

    finished

--- Airport.js
+++ Airport.js
@@ -1,10 +1,11 @@
 class Airport {
 	constructor(name) {
 		this.name = name
-		this.planes = //what would be the best datatype?
+		this.planes = []//what would be the best datatype?
 	}
 
 	addPlane(plane) {
+		this.planes.push(plane)
 		//use an array method
 	}
 }
--- Person.js
+++ Person.js
@@ -1,10 +1,18 @@
 class Person {
+    constructor(name, bags) {
+        this.bags = [];
+        this.name = name;
+    }
     // Include a Constructor Method for Properties: 
         // 1. Name
         // 2. Bags
             // 2a. Bags should contain/reference a data type for storing more than one bag object.
 
     // Include an addBag method
+    addBag(bag) {
+        this.bags.push(bag)
+    }
 }
 
+module.exports = Person;
 //Export the Module
